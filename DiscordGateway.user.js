7ce6e698595331ee29958c44ac944edb69ee8832
Imperator2Toulouse <Imperator2Toulouse@users.noreply.github.com>
2017-09-27 00:59:30 +0200

    Modification du nom du script

--- DiscordGateway.user.js
+++ DiscordGateway.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Discord Gateway
 // @namespace   Discord_Gateway
-// @version     1.0
+// @version     1.2
 // @description Script Ogame
 // @author      I2T
 // @include     /^(http|https)://s.*\.ogame\.gameforge\.com/game/.*$/
@@ -11,18 +11,23 @@
 // @exclude     /^(http|https)://www.*$/
 // @exclude     /^(http|https)://.*ajax=1.*$/
 // @copyright   2017+, You
-// @require     http://code.jquery.com/jquery-1.9.1.min.js
+// @require     http://code.jquery.com/jquery-1.10.2.min.js
 // ==/UserScript==
-*/
+//Version 1.0: Initialisation du script qui averti toutes les FREQ_NOTIF les attaques en cours.
+//Version 1.1: Inclusion d'une alarme programmable via la variable FREQ_ALARM.
+//Version 1.2: Correctif pour éviter d'annoncer ses propres attaques lors d'une AG
+//             Ajout du/des nom(s) d'attaquant*/
 
 /* Configuration */
-var URL_WEBHOOK="";
-var FREQ_NOTIF=10;
+var URL_WEBHOOK="https://discordapp.com/api/webhooks/311088521328852993/WfBZzZwXkX7f2VCgHEFNLUHIWMZ_zVdLLImEjYpI8aUXgsHe2l4acSf--zLGFXAXRbUy";
+var FREQ_NOTIF=2;//en min
+var FREQ_ALARM=2;//en min
 
 /* Declarations */
 var univers = window.location.hostname;
 var username;
 var cur_planet;
+var have_played_alert;
 
 /* Fonctions */
 function rand(a,b) { return Math.floor((Math.random()*(b-a))+a);}
@@ -64,6 +69,14 @@ function get_fleet_movement_id(movement_tag)
             }
         }
     }
+}
+
+
+function get_fleet_movement_style(movement_tag)
+{
+    var style_tag = (movement_tag.getElementsByClassName("countDown hostile").length == 1)?'hostile':'friendly';
+
+    return style_tag;
 }
 
 function get_fleet_movement_time(movement_tag)
@@ -120,17 +133,18 @@ function get_fleet_movements(movement_tags)
     for (var i = 0; i < movement_tags.length; ++i)
     {
         var movement_tag = movement_tags[i];
-
         if (is_movement_tag(movement_tag))
         {
             var fleet_movement = {};
             fleet_movement.fleets = [];
             fleet_movement.id = get_fleet_movement_id(movement_tag);
+            fleet_movement.style = get_fleet_movement_style(movement_tag);
             fleet_movement.time = get_fleet_movement_time(movement_tag);
             fleet_movement.type = get_fleet_movement_type(movement_tag);
             fleet_movement.target_name = get_fleet_movement_target_name(movement_tag);
             fleet_movement.target_type = get_fleet_movement_target_type(movement_tag);
             fleet_movement.target_coordinates = get_fleet_movement_target_coordinates(movement_tag);
+            fleet_movement.origin_username = get_fleet_originname(movement_tag);
 
             fleet_movements.push(fleet_movement);
         }
@@ -163,9 +177,9 @@ function get_fleet_source_coordinates(fleet_tag)
 
 function get_fleet_size(fleet_tag)
 {
-    var details_fleet_tag = fleet_tag.getElementsByClassName("detailsFleet")[0];
+    var size_fleet_tag = fleet_tag.getElementsByClassName("detailsFleet")[0];
     
-    return details_fleet_tag.textContent.trim();
+    return size_fleet_tag.textContent.trim();
 }
 
 function get_fleet_details(fleet_tag)
@@ -176,6 +190,7 @@ function get_fleet_details(fleet_tag)
     if (span_icon_movement_tags.length == 0)
         span_icon_movement_tags = $(fleet_tag).find("td.icon_movement_reserve span");
 
+    var row_tags = $(span_icon_movement_tags.attr('title')).find(".fleetinfo tr");
     var row_tags = $(span_icon_movement_tags.attr('title')).find(".fleetinfo tr");
 
     var category = "";
@@ -206,6 +221,15 @@ function get_fleet_details(fleet_tag)
     return details;
 }
 
+//$(".eventFleet:eq(0) .sendMail a").attr('title')
+function get_fleet_originname(fleet_tag)
+{
+    var originname_fleet_tag = $(fleet_tag).find(".sendMail a").attr('title');
+
+    return originname_fleet_tag;
+}
+
+
 function get_fleets(fleet_tags)
 {
     var fleets = [];
@@ -223,6 +247,7 @@ function get_fleets(fleet_tags)
             fleet.source_coordinates = get_fleet_source_coordinates(fleet_tag);
             fleet.size = get_fleet_size(fleet_tag);
             fleet.details = get_fleet_details(fleet_tag);
+            fleet.origin_username = get_fleet_originname(fleet_tag);
 
             fleets.push(fleet);
         }
@@ -281,14 +306,32 @@ function process_event_list(content)
     for (var i = 0; i < fleet_movements.length; ++i)
     {
         var fleet_movement = fleet_movements[i];
-		//debugger;
-        if ((fleet_movement.type.split("|")[0] == "Flotte ennemie" || fleet_movement.type == "Attaque groupée")
-            && fleet_movement.type.split("|")[1] != "Espionner" && (readCookie('webhook_advert_' + fleet_movement.id, 'all') == null
-                || time() >= parseInt(readCookie('webhook_advert_' + fleet_movement.id, 'all')) + FREQ_NOTIF * 60 * 1000))
-        {
-            send_to_webhook(fleet_movement);
+        if (
+            (
+            fleet_movement.type.split("|")[0] == "Flotte ennemie"
+            || (fleet_movement.type == "Attaque groupée" && fleet_movement.style == "hostile")
+            )
+            && fleet_movement.type.split("|")[1] != "Espionner"
+        ) {
+            if (fleet_movement.type.split("|")[0] == "Flotte ennemie" && have_played_alert != true) {
+                setTimeout(function(){
+                    bruit_alert('http://www.sephiogame.com/script/alert_nuclear_bomb3.ogg');
+                    have_played_alert = true;
+                    setTimeout(function(){have_played_alert = false;},FREQ_ALARM * 60 * 1000);
+                }, 4*1000);
+            }
+            if (readCookie('webhook_advert_' + fleet_movement.id, 'all') == null
+                || time() >= parseInt(readCookie('webhook_advert_' + fleet_movement.id, 'all')) + FREQ_NOTIF * 60 * 1000) {
+                send_to_webhook(fleet_movement);
+                console.log('send to webhook: '+fleet_movement.id);
+            }
         }
     }
+}
+
+function bruit_alert(url) {
+    if ($('#div_for_sound').length < 1) $('#ie_message').append('<div id="div_for_sound"></div>');
+    $('#div_for_sound').html('<audio controls autoplay="true"><source src="'+url+'" type="audio/mpeg" volume="0.5"></audio>');
 }
 
 function check_attack() {
@@ -317,11 +360,12 @@ function send_to_webhook(fleet_movement) {
 	var message = "|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|\n";
     message += "Attaque en cours sur [target_name] [target_coordinates] ([target_type])\n";
     message += "\tDéfenseur : [target_username]\n";
+    message += "\tAttaquant(s) : [origin_username]\n";
     message += "\tHeure d'impact : [impact_date]\n";
     message += "[fleets]";
     message += "|_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|\n";
     
-    var fleet_prototype = "\t\tAttaquant : [source_name] de [source_coordinates] ([source_type])\n";
+    var fleet_prototype = "\t\tAttaquant: \n\t\t\tName: [origin_username]\n\t\t\tSource: [source_name] de [source_coordinates] ([source_type])\n";
     fleet_prototype += "\t\t\tVaisseaux ([size]) :\n";
     fleet_prototype += "[details]";
 
@@ -330,6 +374,7 @@ function send_to_webhook(fleet_movement) {
     {
         var f = fleet_movement.fleets[i];
         var p = fleet_prototype;
+        p = p.replace("[origin_username]", f.origin_username);
         p = p.replace("[source_name]", f.source_name);
         p = p.replace("[source_type]", f.source_type);
         p = p.replace("[source_coordinates]", f.source_coordinates);
@@ -341,11 +386,11 @@ function send_to_webhook(fleet_movement) {
             var d = f.details[j];
             if (d.category == "Chargement")
             {
-                details += "\t\t\t\t" + d.type + " : " + d.count + "\n";
+                details += "\t\t\t\t\t" + d.type + " : " + d.count + "\n";
             }
             else
             {
-                details += "\t\t\t" + d.type + " : " + d.count + "\n";
+                details += "\t\t\t\t" + d.type + " : " + d.count + "\n";
             }
         }
 
@@ -356,6 +401,10 @@ function send_to_webhook(fleet_movement) {
 
     message = message.replace("[target_type]", fleet_movement.target_type);
     message = message.replace("[target_name]", fleet_movement.target_name);
+    //var origin_usernames="";fleet_movement.fleets.forEach(function(fleet){origin_usernames+=(origin_usernames == ""?fleet.origin_username:','+fleet.origin_username);});
+    var origin_usernames=[];fleet_movement.fleets.forEach(function(fleet){origin_usernames.push(fleet.origin_username);});
+    debugger;
+    message = message.replace("[origin_username]", $.unique(origin_usernames).join(", "));
     message = message.replace("[target_coordinates]", fleet_movement.target_coordinates);
     message = message.replace("[target_username]", username);
     message = message.replace("[impact_date]", fleet_movement.time);
@@ -370,8 +419,9 @@ function send_to_webhook(fleet_movement) {
 }
 
 /* get username */
-username=($('span.textBeefy a.overlay.textBeefy').length > 0)?$('span.textBeefy a.overlay.textBeefy').html().replace(/ /g,'').replace("\n",''):"unloged";
-cur_planet=$("#selectedPlanetName").html().trim();
-
+userid=$('head').find('meta[name="ogame-player-id"]').attr("content");
+username=($('head').find('meta[name="ogame-player-name"]').length > 0)?$('head').find('meta[name="ogame-player-name"]').attr("content"):"unloged";
+//cur_planet=$("#selectedPlanetName").html().trim();
+cur_planet=($('head').find('meta[name="ogame-planet-name"]').length > 0)?$('head').find('meta[name="ogame-planet-name"]').attr("content"):"";
 /* Launch */
 setTimeout(check_attack, 2000);
